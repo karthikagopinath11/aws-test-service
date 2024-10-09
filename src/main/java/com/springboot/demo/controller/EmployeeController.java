fea5047f544772961172036bed374817745e0c3c
KChankaramkulath <KChankaramkulath@korewireless.com>
2024-10-09 13:29:42 +0530

    Test commit

--- src/main/java/com/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/springboot/demo/controller/EmployeeController.java
@@ -35,7 +35,7 @@ public class EmployeeController {
 
 	//Save employee
 	@PostMapping("/saveEmployee")
-	@Operation(summary = "Saves the employee details--New change", description = "Returns true if successfully saved")
+	@Operation(summary = "Saves the employee details --New change", description = "Returns true if successfully saved")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Successfully saved"),
 	})
